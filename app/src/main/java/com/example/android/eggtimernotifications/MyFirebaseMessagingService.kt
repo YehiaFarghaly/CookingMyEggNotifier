8aff0015ab60d83af1daa43a48a19717f22ef4c3
Yehia <yehia71691@gmail.com>
2022-09-10 18:23:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,23 @@
+//package com.example.android.eggtimernotifications
+//
+//import android.util.Log
+//import com.google.firebase.messaging.FirebaseMessagingService
+//import com.google.firebase.messaging.RemoteMessage
+//
+//class MyFirebaseMessagingService:FirebaseMessagingService() {
+//
+//    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+//        super.onMessageReceived(remoteMessage)
+//
+//        // TODO step 3.5 check messages for data
+//
+//
+//        // TODO 3.6 check messages for notification
+//    }
+//
+//    override fun onNewToken(token: String?) {
+//        Log.d("token",token!!)
+//
+//        super.onNewToken(token)
+//    }
+//}
